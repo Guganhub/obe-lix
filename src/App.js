ddb64a58d95892527f9c9c47b106e68a3cf0f439
unknown <guganesh12345@gmail.com>
2022-11-26 15:47:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
